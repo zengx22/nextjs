2b7d460d84b3b7a77e1e04f38cbe7ce8fcf44712
Xiao Zeng <zengx22@hotmail.com>
2024-06-09 00:22:22 -0400

    Register new user - working

--- redux/store.ts
+++ redux/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './features/userSlice'
+import { authApi } from './features/api/authApi'
 
 export const store = configureStore({
   reducer: {
     auth: userReducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([authApi.middleware]),
 })
 
 export type RootState = ReturnType<typeof store.getState>
