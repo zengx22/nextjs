90793a8f3dcacfd54a70eca932d7b4137eafdb0e
Xiao Zeng <zengx22@hotmail.com>
2024-06-10 23:59:44 -0400

    Backend setup for upload avatar with Cloudinary.

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { catchAsyncErrors } from '../middlewares/catchAsyncErrors'
 import User from '../models/user'
 import ErrorHandler from '../utils/errorHandler'
+import { delete_file, upload_file } from '../utils/cloudinary'
 
 export const registerUser = catchAsyncErrors(async (req: NextRequest) => {
   const body = await req.json()
@@ -47,5 +48,25 @@ export const updatePassword = catchAsyncErrors(async (req: NextRequest) => {
 
   return NextResponse.json({
     success: true,
+  })
+})
+
+export const uploadAvatar = catchAsyncErrors(async (req: NextRequest) => {
+  const body = await req.json()
+
+  const avatarResponse = await upload_file(body?.avatar, 'bookit/avatar')
+
+  // Remove existing avatar
+  if (req?.user?.avatar.public_id) {
+    delete_file(req?.user?.avatar.public_id)
+  }
+
+  const user = await User.findByIdAndUpdate(req?.user?._id, {
+    avatar: avatarResponse,
+  })
+
+  return NextResponse.json({
+    success: true,
+    user,
   })
 })
--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ const nextConfig = {
     DB_LOCAL_URI: 'mongodb://127.0.0.1:27017/bookit-v2',
     DB_URI: '',
 
+    CLOUDINARY_CLOUD_NAME: 'ddo5hqvll',
+    CLOUDINARY_API_KEY: '447895363791952',
+    CLOUDINARY_API_SECRET: 'VGsJnCuYdu7_9dSJlZR9wWQnd3Y',
+
     NEXTAUTH_URL: 'http://localhost:3000',
     NEXTAUTH_SECRET: 'HDTHFDHRJHNFBDSEFWTHHFBDF?',
   },
