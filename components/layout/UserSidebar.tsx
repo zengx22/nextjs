98927e29784be97cc07643dbc8fe02dfbe8e3135
Xiao Zeng <zengx22@hotmail.com>
2024-06-11 19:37:11 -0400

    Fix user sidebar menu and update nextjs version

--- components/layout/UserSidebar.tsx
+++ components/layout/UserSidebar.tsx
@@ -35,10 +35,10 @@ const UserSidebar = () => {
           key={index}
           href={menuItem.url}
           className={`fw-bold list-group-item list-group-item-action ${
-            activeMenuItem === menuItem.name ? 'active' : ''
+            activeMenuItem === menuItem.url ? 'active' : ''
           }`}
-          onClick={() => handleMenuItemClick(menuItem.name)}
-          aria-current={activeMenuItem === menuItem.name ? 'true' : 'false'}
+          onClick={() => handleMenuItemClick(menuItem.url)}
+          aria-current={activeMenuItem === menuItem.url ? 'true' : 'false'}
         >
           <i className={`${menuItem.icon} fa-fw pe-2`}></i> {menuItem.name}
         </Link>
