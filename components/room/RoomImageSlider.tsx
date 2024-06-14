78c1105fdbdd379784d5385a3a4e53cd9a7b615b
Xiao Zeng <zengx22@hotmail.com>
2024-06-13 20:59:45 -0400

    Fix a small mistake for displaying default image of room details.

--- components/room/RoomImageSlider.tsx
+++ components/room/RoomImageSlider.tsx
@@ -28,8 +28,8 @@ const RoomImageSlider = ({ images }: Props) => {
           <div style={{ widows: '100%', height: '460px' }}>
             <Image
               className='d-block m-auto'
-              src={'../images/default_room_image.jpg'}
-              alt={'../images/default_room_image.jpg'}
+              src={'/images/default_room_image.jpg'}
+              alt={'/images/default_room_image.jpg'}
               layout='fill'
             />
           </div>
