19fc15677850ada00900827bb4fc155d7d90f153
Xiao Zeng <zengx22@hotmail.com>
2024-06-19 06:56:12 -0400

    Fix geoCode by importing 'node-fetch' - worked

--- backend/utils/geoCoder.ts
+++ backend/utils/geoCoder.ts
@@ -1,11 +1,11 @@
+import fetch from 'node-fetch'
 const NodeGeocoder = require('node-geocoder')
 
 const options = {
+  fetch: fetch,
   provider: process.env.GEOCODER_PROVIDER,
   apiKey: process.env.GEOCODER_API_KEY,
   formatter: null,
 }
-
 const geoCoder = NodeGeocoder(options)
-
 export default geoCoder
